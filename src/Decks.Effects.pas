249cb0add668f78a852e43325791727d6581ef70
hukka <hukkax@gmail.com>
2022-09-02 20:50:42 +0300

    tweak effects

--- src/Decks.Effects.pas
+++ src/Decks.Effects.pas
@@ -15,6 +15,8 @@ type
 	TFilterBand = ( FILTER_LOWPASS, FILTER_HIGHPASS );
 
 const
+	MUL_DEFAULT = 100;
+
 	CenterFreqs: array[TEQBand] of Word = (125, 1000, 8000);
 
 	STR_MIX_DRY = 'Dry';
@@ -56,8 +58,9 @@ type
 		procedure   SetValue(AValue: Single);
 	public
 		Name,
-		Caption:  String;
-		Result:   Integer;
+		Caption:    String;
+		Result:     Integer;
+		Multiplier: Integer;
 
 		OnChange: TEffectParamChangeEvent;
 
@@ -96,7 +99,7 @@ type
 		Presets:    TEffectPresets;
 
 		function    AddParam(var ValuePtr: Single; const AName: String;
-		            AMin, AMax: Single; const ACaption: String): TEffectParam;
+		            AMin, AMax: Single; AMultiplier: Integer; const ACaption: String): TEffectParam;
 		procedure   AddPreset(const Values: array of Single; const AName: String);
 		procedure   ApplyPreset(Preset: TEffectPreset);
 		procedure   ParamChanged(Param: TEffectParam);
@@ -210,11 +213,12 @@ begin
 end;
 
 function TBaseEffect.AddParam(var ValuePtr: Single; const AName: String;
-	AMin, AMax: Single; const ACaption: String): TEffectParam;
+	AMin, AMax: Single; AMultiplier: Integer; const ACaption: String): TEffectParam;
 begin
 	Result := TEffectParam.Create(AName, ACaption, AMin, AMax, ValuePtr);
 	if Result <> nil then
 	begin
+		Result.Multiplier := AMultiplier;
 		Result.OnChange := ParamChanged;
 		Params.Add(Result);
 	end;
@@ -302,11 +306,11 @@ begin
 	inherited Create(BASS_FX_BFX_ECHO4, @BFX, 'Echo');
 	BFX.lChannel := BASS_BFX_CHANALL;
 
-	AddParam(BFX.fDryMix,	STR_MIX_DRY,	+0.0,	+1.0,	STR_MIX_DRY_DESC);
-	AddParam(BFX.fWetMix,	STR_MIX_WET,	+0.0,	+1.0,	STR_MIX_WET_DESC);
+	AddParam(BFX.fDryMix,	STR_MIX_DRY,	+0.0,	+1.0,	MUL_DEFAULT,	STR_MIX_DRY_DESC);
+	AddParam(BFX.fWetMix,	STR_MIX_WET,	+0.0,	+1.0,	MUL_DEFAULT,	STR_MIX_WET_DESC);
 
-	AddParam(BFX.fFeedback,	'Feedback', -1.0,	+1.0,	STR_MIX_FEEDBACK);
-	AddParam(BFX.fDelay,	'Delay',	+0.0,	+10.0,	'Delay in seconds');
+	AddParam(BFX.fFeedback,	'Feedback', 	-1.0,	+1.0,	MUL_DEFAULT,	STR_MIX_FEEDBACK);
+	AddParam(BFX.fDelay,	'Delay',		+0.0,	+1.0,	MUL_DEFAULT,	'Delay in seconds');
 
 	AddPreset([1.0,	1.0,  0.0, 0.2], 'Small Echo');
 	AddPreset([1.0,	1.0,  0.7, 0.5], 'Many Echoes');
@@ -321,12 +325,12 @@ begin
 	inherited Create(BASS_FX_BFX_FREEVERB, @BFX, 'Reverb');
 	BFX.lChannel := BASS_BFX_CHANALL;
 
-	AddParam(BFX.fDryMix,	STR_MIX_DRY,	+0.0,	+1.0,	STR_MIX_DRY_DESC);
-	AddParam(BFX.fWetMix,	STR_MIX_WET,	+0.0,	+1.0,	STR_MIX_WET_DESC);
+	AddParam(BFX.fDryMix,	STR_MIX_DRY,	+0.0,	+1.0,	MUL_DEFAULT,	STR_MIX_DRY_DESC);
+	AddParam(BFX.fWetMix,	STR_MIX_WET,	+0.0,	+1.0,	MUL_DEFAULT,	STR_MIX_WET_DESC);
 
-	AddParam(BFX.fRoomSize,	'RoomSize',	+0.0,	+1.0,	'Room size');
-	AddParam(BFX.fDamp,		'Damp',		+0.0,	+1.0,	'Damping');
-	AddParam(BFX.fWidth,	'Width',	+0.0,	+1.0,	'Stereo width');
+	AddParam(BFX.fRoomSize,	'Size',		+0.0,	+1.0,	MUL_DEFAULT,	'Room size');
+	AddParam(BFX.fDamp,		'Damp',		+0.0,	+1.0,	MUL_DEFAULT,	'Damping');
+	AddParam(BFX.fWidth,	'Width',	+0.0,	+1.0,	MUL_DEFAULT,	'Stereo width');
 
 	AddPreset([1.0, 1.0, 0.5, 0.5, 0.5], 'Default');
 
@@ -338,13 +342,13 @@ begin
 	inherited Create(BASS_FX_BFX_PHASER, @BFX, 'Phaser');
 	BFX.lChannel := BASS_BFX_CHANALL;
 
-	AddParam(BFX.fDryMix,	STR_MIX_DRY,	+0.0,	+2.0,	STR_MIX_DRY_DESC);
-	AddParam(BFX.fWetMix,	STR_MIX_WET,	+0.0,	+2.0,	STR_MIX_WET_DESC);
+	AddParam(BFX.fDryMix,	STR_MIX_DRY,	+0.0,	+2.0,	MUL_DEFAULT,	STR_MIX_DRY_DESC);
+	AddParam(BFX.fWetMix,	STR_MIX_WET,	+0.0,	+2.0,	MUL_DEFAULT,	STR_MIX_WET_DESC);
 
-	AddParam(BFX.fFeedback,	'Feedback', -1.0,	+1.0,		STR_MIX_FEEDBACK);
-	AddParam(BFX.fRate,		'Rate',		+0.0,	+10.0,		'Rate of sweep in cycles per second');
-	AddParam(BFX.fRange,	'Range',	+0.0,	+10.0,		'Sweep range in octaves');
-	AddParam(BFX.fFreq,		'Freq',		+0.0,	+1000.0,	'Base frequency of sweep range');
+	AddParam(BFX.fFeedback,	'Feedback', -1.0,	+1.0,		MUL_DEFAULT,	STR_MIX_FEEDBACK);
+	AddParam(BFX.fRate,		'Rate',		+0.0,	+10.0,		MUL_DEFAULT,	'Rate of sweep in cycles per second');
+	AddParam(BFX.fRange,	'Range',	+0.0,	+10.0,		MUL_DEFAULT,	'Sweep range in octaves');
+	AddParam(BFX.fFreq,		'Freq',		+0.0,	+1000.0,	MUL_DEFAULT,	'Base frequency of sweep range');
 
 	AddPreset([1.0,	 1.0,	 0.0,	1.0,	4.0,	100.0],	'Phase shift');
 	//AddPreset([1.0,	-1.0,	-0.6, 	0.2, 	6.0, 	100.0],	'Slow invert phase shift with feedback');
@@ -363,13 +367,13 @@ begin
 	inherited Create(BASS_FX_BFX_CHORUS, @BFX, 'Chorus');
 	BFX.lChannel := BASS_BFX_CHANALL;
 
-	AddParam(BFX.fDryMix,	STR_MIX_DRY,	-2.0,	+2.0,	STR_MIX_DRY_DESC);
-	AddParam(BFX.fWetMix,	STR_MIX_WET,	-2.0,	+2.0,	STR_MIX_WET_DESC);
+	AddParam(BFX.fDryMix,	STR_MIX_DRY,	-2.0,	+2.0,	MUL_DEFAULT,	STR_MIX_DRY_DESC);
+	AddParam(BFX.fWetMix,	STR_MIX_WET,	-2.0,	+2.0,	MUL_DEFAULT,	STR_MIX_WET_DESC);
 
-	AddParam(BFX.fFeedback,	'Feedback', -1.0,	+1.0,		STR_MIX_FEEDBACK);
-	AddParam(BFX.fMinSweep,	'Min',	+0.0,	+6000.0,		'Minimum delay in ms');
-	AddParam(BFX.fMaxSweep,	'Max',	+0.0,	+6000.0,		'Maximum delay in ms');
-	AddParam(BFX.fRate,		'Rate',		+0.0,	+1000.0,	'Rate in ms/s');
+	AddParam(BFX.fFeedback,	'Feedback', -1.0,	+1.0,		MUL_DEFAULT,	STR_MIX_FEEDBACK);
+	AddParam(BFX.fMinSweep,	'Min',		+0.0,	+600.0,		1,				'Minimum delay in ms');
+	AddParam(BFX.fMaxSweep,	'Max',		+0.0,	+600.0,		1,				'Maximum delay in ms');
+	AddParam(BFX.fRate,		'Rate',		+0.0,	+100.0,		1,				'Rate in ms/s');
 
 	AddPreset([1.0,	 0.35,	0.5,	1.0,	  5.0,	  1.0],	'Flanger');
 	AddPreset([0.7,	 0.25,	0.5,	1.0,	200.0,	 50.0],	'Exaggerated');
@@ -388,12 +392,12 @@ begin
 	inherited Create(BASS_FX_BFX_DISTORTION, @BFX, 'Distortion');
 	BFX.lChannel := BASS_BFX_CHANALL;
 
-	AddParam(BFX.fDryMix,	STR_MIX_DRY,	-5.0,	+5.0,	STR_MIX_DRY_DESC);
-	AddParam(BFX.fWetMix,	STR_MIX_WET,	-5.0,	+5.0,	STR_MIX_WET_DESC);
+	AddParam(BFX.fDryMix,	STR_MIX_DRY,	-5.0,	+5.0,	MUL_DEFAULT,	STR_MIX_DRY_DESC);
+	AddParam(BFX.fWetMix,	STR_MIX_WET,	-5.0,	+5.0,	MUL_DEFAULT,	STR_MIX_WET_DESC);
 
-	AddParam(BFX.fDrive,	'Drive',	+0.0,	+5.0,	'Distortion drive');
-	AddParam(BFX.fFeedback,	'Feedback', -1.0,	+1.0,	STR_MIX_FEEDBACK);
-	AddParam(BFX.fVolume,	'Volume',	+0.0,	+2.0,	'Distortion volume');
+	AddParam(BFX.fDrive,	'Drive',	+0.0,	+5.0,	MUL_DEFAULT,	'Distortion drive');
+	AddParam(BFX.fFeedback,	'Feedback', -1.0,	+1.0,	MUL_DEFAULT,	STR_MIX_FEEDBACK);
+	AddParam(BFX.fVolume,	'Volume',	+0.0,	+2.0,	MUL_DEFAULT,	'Distortion volume');
 
 	AddPreset([-2.95,	-0.05,	0.0,	-0.18,	0.25],	'Soft');
 	AddPreset([ 1.00,	 0.2,	1.0,	 0.10, 	1.00],	'Medium');
@@ -408,15 +412,15 @@ begin
 	inherited Create(BASS_FX_BFX_COMPRESSOR2, @BFX, 'Compressor');
 	BFX.lChannel := BASS_BFX_CHANALL;
 
-	AddParam(BFX.fGain,			'Gain',			+0.0,	+60.0,
+	AddParam(BFX.fGain,			'Gain',			+0,		+60,	MUL_DEFAULT,
 		'Output gain of signal after compression');
-	AddParam(BFX.fThreshold,	'Threshold',	-60.0,	+0.0,
+	AddParam(BFX.fThreshold,	'Threshold',	-60,	+0,		MUL_DEFAULT,
 		'Point at which compression begins');
-	AddParam(BFX.fRatio,		'Ratio',		+1.0,	+100.0,
+	AddParam(BFX.fRatio,		'Ratio',		+1,		+100,	MUL_DEFAULT,
 		'Compression ratio');
-	AddParam(BFX.fAttack,		'Attack',		+0.01,	+500.0,
+	AddParam(BFX.fAttack,		'Attack',		+0.01,	+500,	500,
 		'Time before compression reaches its full value');
-	AddParam(BFX.fRelease,		'Release',		+0.01,	+5000.0,
+	AddParam(BFX.fRelease,		'Release',		+0.01,	+5000,	5000,
 		'Speed at which compression is stopped after input drops below Threshold');
 
 	AddPreset([5, -15, 3, 20, 500],	'Default');
